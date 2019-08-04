63f963c6b61aa5a11d0cdd2ed5b353c59115e381
eqs <murashige.satoshi.mi1@is.naist.jp>
2019-08-04 23:01:17 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import {
+  JupyterFrontEnd, JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+
+/**
+ * Initialization data for the jupyterlab-astronomy extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-astronomy',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension jupyterlab-astronomy is activated!');
+  }
+};
+
+export default extension;
